c9471b203e61c0a86d7879eac918fa29c4b8fe66
Junsik Hwang <js.seth.h@gmail.com>
2017-11-24 16:37:38 +0900

    브라우저에서 충돌나서 수정.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 
-if(require.extensions['.coffee'])
-  module.exports = require('./lib')
+support_coffee = undefined
+if ((support_coffee === undefined) && (typeof process !== "undefined" && process !== null) && ((typeof require !== "undefined" && require !== null ? require.extensions : void 0) != null) && require.extensions['.coffee']) {
+  support_coffee = true
+}else {
+  support_coffee = false
+}
+
+if(support_coffee)
+  module.exports = require('./lib');
 else
-  module.exports = require('./lib-js')
+  module.exports = require('./lib-js');
+
